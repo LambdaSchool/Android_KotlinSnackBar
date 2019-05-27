a1c0e82565a12dace2fe2265964461d73d769fcf
Jake Esveld <thejakeesveld@gmail.com>
2019-05-27 12:21:05 -0700

    Instantiate objects

--- src/Main.kt
+++ src/Main.kt
@@ -1,5 +1,17 @@
 fun main(args: Array<String>) {
     println("Hello World")
+
+    val Jane = Customer(1, "Jane", 45.25)
+    val Bob = Customer(2, "Bob", 33.14)
+    val FoodVM = VendingMaching("Food", 1)
+    val DrinkVM = VendingMaching("Drink", 2)
+    val OfficeVM = VendingMaching("Office", 3)
+    val Chips = Snack(1, "Chips", 36, 1.75, 1)
+    val ChocolatBar = Snack(2, "Chocolate Bar", 36, 1.00, 1)
+    val Pretzel = Snack(3, "Pretzel", 30, 2.00, 1)
+    val Soda = Snack(4, "Soda", 24, 2.50, 2)
+    val Water = Snack(5, "Water", 20, 2.75, 2)
+
 }
 
 class Snack(val id: Int, var name: String, private var quantity: Int, var cost: Double, val vmId: Int){

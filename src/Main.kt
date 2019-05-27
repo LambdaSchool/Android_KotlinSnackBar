3a20135236535e6a1acbd729492de8c1b9cce107
Jake Esveld <thejakeesveld@gmail.com>
2019-05-27 14:04:50 -0700

    Add Processes

--- src/Main.kt
+++ src/Main.kt
@@ -12,15 +12,40 @@ fun main(args: Array<String>) {
     val Soda = Snack(4, "Soda", 24, 2.50, 2)
     val Water = Snack(5, "Water", 20, 2.75, 2)
 
+    Jane.buy(Soda.buySnack(3))
+    println(Jane.cashOnHand)
+    println(Soda.quantity)
+
+    Jane.buy(Pretzel.buySnack(1))
+    println(Jane.cashOnHand)
+    println(Pretzel.quantity)
+
+    Bob.buy(Soda.buySnack(2))
+    println(Bob.cashOnHand)
+    println(Soda.quantity)
+
+    Jane.addCash(10.0)
+    println(Jane.cashOnHand)
+
+    Jane.buy(ChocolatBar.buySnack(1))
+    println(Jane.cashOnHand)
+    println(ChocolatBar.quantity)
+
+    Pretzel.addQuantity(12)
+    println(Pretzel.quantity)
+
+    Bob.buy(Pretzel.buySnack(3))
+    println(Bob.cashOnHand)
+    println(Pretzel.quantity)
 }
 
-class Snack(val id: Int, var name: String, private var quantity: Int, var cost: Double, val vmId: Int){
+class Snack(val id: Int, var name: String, var quantity: Int, var cost: Double, val vmId: Int){
     fun addQuantity(addedQuantity: Int){
-        quantity.plus(addedQuantity)
+        quantity = quantity.plus(addedQuantity)
     }
 
     fun buySnack(amountBought: Int): Double{
-        quantity.minus(amountBought)
+        quantity = quantity.minus(amountBought)
         return amountBought.times(cost)
     }
 
@@ -33,11 +58,11 @@ class VendingMaching(var name: String, val id: Int)
 
 class Customer(val id: Int, val name: String, var cashOnHand: Double){
     fun addCash(addedCash: Double){
-        cashOnHand.plus(addedCash)
+        cashOnHand = cashOnHand.plus(addedCash)
     }
 
     fun buy(cost: Double){
-        cashOnHand.minus(cost)
+        cashOnHand = cashOnHand.minus(cost)
     }
     val isBroke: Boolean
         get() = cashOnHand <= 0.0
